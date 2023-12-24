c7f4fb813161408e9f331eaeb8cb9849117b3c33
mau <mauriciotxai@gmail.com>
2023-12-24 12:27:20 -0600

    Inclusión de loggers personalizados

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +12,12 @@ func main() {
 	//En go es común realizar "banderas" para mejorar las variables que vamos a ocupar
 	// se puede usar -help en cmd
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	//Infolog es un mensaje que aparece en terminal con informacion relevante
+	infoLog := log.New(os.Stdout, "INFO---\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR---\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	//Aqui instanciamos el ServerMux; su función es mapear el patrón
 	// URL con la función.
 	mux := http.NewServeMux()
@@ -22,7 +29,7 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	//Activamos el server en el puerto 4001
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
